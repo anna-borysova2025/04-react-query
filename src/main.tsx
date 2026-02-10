82033d378af667c2f49095e013640edaf910953b
anna_borysova <anna@pavlyuk.org>
2026-02-10 13:06:45 +0200

    final upd

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
-// import { createRoot } from 'react-dom/client';
-import App from './components/App/App';
-import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+const queryClient = new QueryClient();
 
+import { createRoot } from 'react-dom/client';
+import App from './components/App/App';
 
-const queryClient = new QueryClient()
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-<React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
     <App />
     <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider> 
-</React.StrictMode>
+  </QueryClientProvider>,
 );
